7ff7dbded60ff555c60c31a0809e8815b43d7d5e
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2019-08-15 21:46:58 -0700

    Updated Code to latest release
    - Working Send and Receive functions
    - Squashed lots of bugs
    - Added a working example / test

--- TestFileTrans.py
+++ TestFileTrans.py
@@ -1,7 +1,45 @@
 import FileTransfer as ft
 import socket
+import threading
+
+def AcceptNewClients(s, ClientList, BufferSize):
+    while True:
+        conn, addr = s.accept()
+        ClientList.append(conn)
+        NewThread = threading.Thread(target=TransferMessage, kwargs={"s":conn, "ClientList":ClientList, "BufferSize":BufferSize})
+        NewThread.start()
+
+def TransferMessage(s, ClientList, BufferSize):
+    while True:
+        try:
+            ReceivedMessages = s.recv(BufferSize)
+        except Exception as err:
+            print(err)
+            return
+        for ClientSockt in ClientList:
+            if ClientSockt != s:
+                try:
+                    ClientSockt.send(ReceivedMessages)
+                except Exception as ErrSend:
+                    print(ErrSend)
+                    return
+
+ClientList = []
+Buffersize = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.1.144", 32123))
-print("Connected")
-ft.Send(filepath="~/Downloads/FileTransfer-master/README.md", socket=s)
+s.bind(("0.0.0.0", 32123))
+s.listen(10)
+
+ServerThread = threading.Thread(target=AcceptNewClients, kwargs={"s":s, "ClientList":ClientList, "BufferSize":Buffersize})
+ServerThread.daemon = True
+ServerThread.start()
+
+ClientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ClientSock.connect(("127.0.0.1", 32123))
+print("Connected!")
+
+ft.Receive("C:\\Users\\Sid\\Documents\\Codes\\FTUniversal\\", ClientSock)
+while True:
+    pass
+    
